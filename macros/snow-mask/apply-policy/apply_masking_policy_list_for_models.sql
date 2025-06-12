0290e19a4fc82a717929f1fa8eabc0f0cc5d75ac
Rebecca <rubyDoomsday@users.noreply.github.com>
2025-06-12 13:35:16 -0400

    Apply mask to quoted columns (#81)
    * Apply mask to quoted columns
    
    **Description**
    
    This update addresses the issue identified by
    https://github.com/entechlog/dbt-snow-mask/issues/80 where using a
    quoted column breaks the mask SQL preventing the ability to apply
    masking to columns with spaces in the name.
    
    * Update macros/snow-mask/apply-policy/apply_masking_policy_list_for_models.sql
    
    Co-authored-by: Jose Escudero <9829156+josescuderoh@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: entechlog <38563714+entechlog@users.noreply.github.com>
    Co-authored-by: Jose Escudero <9829156+josescuderoh@users.noreply.github.com>

--- macros/snow-mask/apply-policy/apply_masking_policy_list_for_models.sql
+++ macros/snow-mask/apply-policy/apply_masking_policy_list_for_models.sql
@@ -51,6 +51,7 @@
 
             {%- for meta_tuple in meta_columns if meta_columns | length > 0 %}
                 {% set column               = meta_tuple[0] %}
+                {% set quoted_column = '"' ~ column ~ '"' if model.columns.get(column, {}).get('quote', false) else column %}
                 {% set masking_policy_name  = meta_tuple[1] %}
                 {% set conditional_columns  = meta_tuple[2] %}
 
@@ -61,8 +62,8 @@
                             {{ log(modules.datetime.datetime.now().strftime("%H:%M:%S") ~ " | " ~ operation_type ~ "ing masking policy to model  : " ~ masking_policy_db|upper ~ '.' ~ masking_policy_schema|upper ~ '.' ~ masking_policy_name|upper ~ " on " ~ database ~ '.' ~ schema ~ '.' ~ alias ~ '.' ~ column ~ ' [force = ' ~ var('use_force_applying_masking_policy','False') ~ ']', info=True) }}
                             {% set query %}
                             alter {{materialization}} {{database}}.{{schema}}.{{alias}}
-                            modify column {{column}}
-                            set masking policy {{masking_policy_db}}.{{masking_policy_schema}}.{{masking_policy_name}} {% if conditional_columns | length > 0 %}using ({{column}}, {{conditional_columns|join(', ')}}){% endif %} {% if var('use_force_applying_masking_policy','False')|upper in ['TRUE','YES'] %} force {% endif %};
+                            modify column {{quoted_column}}
+                            set masking policy {{masking_policy_db}}.{{masking_policy_schema}}.{{masking_policy_name}} {% if conditional_columns | length > 0 %}using ({{quoted_column}}, {{conditional_columns|join(', ')}}){% endif %} {% if var('use_force_applying_masking_policy','False')|upper in ['TRUE','YES'] %} force {% endif %};
                             {% endset %}
                             {% do run_query(query) %}
                         {% endif %}
@@ -95,12 +96,13 @@
 
                 {%- for meta_tuple in meta_columns if meta_columns | length > 0 %}
                     {% set column   = meta_tuple[0] %}
+                    {% set quoted_column = '"' ~ column ~ '"' if node.columns.get(column, {}).get('quote', false) else column %}
                     {% set masking_policy_name  = meta_tuple[1] %}
 
                     {% if masking_policy_name is not none %}
                         {{ log(modules.datetime.datetime.now().strftime("%H:%M:%S") ~ " | " ~ operation_type ~ "ing masking policy to model  : " ~ database|upper ~ '.' ~ schema|upper ~ '.' ~ masking_policy_name|upper ~ " on " ~ database ~ '.' ~ schema ~ '.' ~ alias ~ '.' ~ column, info=True) }}
                         {% set query %}
-                            alter {{materialization}}  {{database}}.{{schema}}.{{alias}} modify column  {{column}} unset masking policy
+                            alter {{materialization}}  {{database}}.{{schema}}.{{alias}} modify column  {{quoted_column }} unset masking policy
                         {% endset %}
                         {% do run_query(query) %}
                     {% endif %}
